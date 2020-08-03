a184cd7c15ce2515cdf0fa6a62c997e05c90ac95
Prashant Gaur <prasgaur@cloudlock.com>
2020-08-04 01:56:16 +0530

    done with feature scaling

--- ML/data_preprocessing/try_1.py
+++ ML/data_preprocessing/try_1.py
@@ -8,11 +8,13 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 le = LabelEncoder()
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+sc = StandardScaler()
 
 
 def get_dataset():
@@ -35,17 +37,26 @@ def feature_transform(features):
 
 def dep_var_encoding(dep_var):
     dep_var = le.fit_transform(dep_var)
+    return dep_var
 
 
 def split_data(features, dep_var):
     return train_test_split(features, dep_var, test_size=0.2, random_state=1)
 
 
+def feature_scaling(data):
+    data[:, 3:] = sc.fit_transform(data[:, 3:])
+
 if __name__ == '__main__':
     """
     """
     features, dep_var = get_dataset()
     features = feature_transform(features)
-    dep_var_encoding = dep_var_encoding(dep_var)
-    X_train, X_test, y_train, y_test = split_data(features, dep_var)
-    print(X_train, X_test, y_train, y_test)
+    dep_var = dep_var_encoding(dep_var)
+    features_train, features_test, dep_var_train,\
+        dep_var_test = split_data(features, dep_var)
+
+    feature_scaling(features_train)
+    feature_scaling(features_test)
+
+    print(features_train)
